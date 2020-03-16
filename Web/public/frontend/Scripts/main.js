8123fc0291bf61ccbcca66cf2f6b7a24b2f152bf
mcgodamn <t7214948ttt@gmail.com>
2020-03-17 01:29:47 +0800

    client can show data now

--- Web/public/frontend/Scripts/main.js
+++ Web/public/frontend/Scripts/main.js
@@ -17,6 +17,9 @@ var vm = new Vue({
                 this._isCrawling = crawling
             }
         },
+        crawl_result: {
+        },
+        crawl_result_header: [],
     },
     methods: {
         StartCrawler() {
@@ -27,11 +30,50 @@ var vm = new Vue({
                     event:"StartCrawler"
                 },
                 { emulateJSON: true }).then(function (res) {
-                    console.log(res.body)
+                    handleData(res.body)
                 }, function (res) {
                     console.log(res.status)
                     console.log(res.body)
                 })
+        },
+        getHeaderHtml() {
+            var str = `<th scope="col">#</th>`
+            for (var d in this.crawl_result_header) {
+                str += `<th scope="col">${this.crawl_result_header[d]}</th>`
+            }
+            return this.crawl_result_header.length == 0 ? "" : str
+        },
+        getDataHtml() {
+            var html = ""
+            var i = 1
+            for (var res in this.crawl_result)
+            {
+                var str = `<th scope="row">${i++}</th>`
+                var data = this.crawl_result[res]
+                for (var d in data) {
+                    str += (`<td>${data[d]}</td>`)
+                }
+                html += `<tr>${str}</tr>`
+            }
+            return this.crawl_result.length == 0 ? "" : html
         }
     },
 });
+
+function handleData(data)
+{
+    console.log(data)
+    if (data.length == 0) {return}
+    var result = data[0]
+    var head = []
+    for (var first_key in result)
+    {
+        for (var key in result[first_key])
+        {
+            head.push(key)
+        }
+        break
+    }
+    vm.crawl_result_header = head
+    vm.crawl_result = result
+}
