f671125429b74a38fa64170bd2a03fa629754eb6
MJChung <t7214948ttt@gmail.com>
2020-03-19 18:37:33 +0800

    generate html code by vue

--- Web/public/frontend/Scripts/main.js
+++ Web/public/frontend/Scripts/main.js
@@ -1,3 +1,8 @@
+import {
+    TYPE,
+    PARAMETERS
+} from './data.js'
+
 var socket = io('http://localhost:3000');
 socket.on('connect', function () {
     socket.emit('whoamI',"client")
@@ -29,6 +34,13 @@ var vm = new Vue({
         crawl_result_header: [],
     },
     methods: {
+        getOptionPageHtml() {
+            var html = ""
+            for (var para in PARAMETERS) {
+                html += getDataHtml(PARAMETERS[para])
+            }
+            return html
+        },
         StartCrawler() {
             if (this.state.isCrawling) return
             socket.emit("command",
@@ -42,14 +54,14 @@ var vm = new Vue({
                 }
             )
         },
-        getHeaderHtml() {
+        getTableHeaderHtml() {
             var str = `<th scope="col">#</th>`
             for (var d in this.crawl_result_header) {
                 str += `<th scope="col">${this.crawl_result_header[d]}</th>`
             }
             return this.crawl_result_header.length == 0 ? "" : str
         },
-        getDataHtml() {
+        getTableDataHtml() {
             var html = ""
             var i = 1
             for (var res in this.crawl_result)
@@ -82,4 +94,103 @@ function handleData(data)
     }
     vm.crawl_result_header = head
     vm.crawl_result = result
+}
+
+function getDataHtml(_para)
+{
+    var res = ""
+    switch (_para.type)
+    {
+        case TYPE.OPTIONS:
+            res = getOptionHtml(_para)
+            break
+
+        case TYPE.RANGE:
+            res = getRangeHtml(_para)
+            break
+
+        case TYPE.TWO_LEVEL_OPTIONS:
+            res = get2LevelOptionHtml(_para)
+            break
+        default:
+            break
+    }
+
+    return res
+}
+
+function getOptionHtml(_para){
+    var res = `<div class="form-group row text-center">
+                    <div class="col">
+                        <h3 class="row text-right pl-1 accordion-toggle" data-toggle="collapse" href="#collapse_${_para.name}"
+                            role="button" aria-expanded="false" aria-controls="collapse_${_para.name}">${_para.text}</h3>
+                        <div class="row collapse show" id="collapse_${_para.name}">`
+    for (var key in _para.data){
+        var data = _para.data[key]
+        res +=`<div class="form-check form-check-inline">
+                    <input class="form-check-input" type="checkbox" id="${key}" v-model="${key} value=${data.value}">
+                    <label class="form-check-label" for="${key}">${data.text}</label>
+                </div>`
+    }
+    res += `</div>
+        </div>
+    </div>`
+    return res
+}
+function getRangeHtml(_para) {
+    return `
+    <div class="form-group row text-center">
+    <div class="col">
+        <h3 class="row text-right pl-1 accordion-toggle" data-toggle="collapse"
+            href="#collapse_${_para.name}" role="button" aria-expanded="false" aria-controls="collapse_${_para.name}">
+            ${_para.text}</h3>
+        <div class="row collapse show" id="collapse_${_para.name}">
+            <input type="text" class="form-control col col-lg-3" placeholder="下限"
+                v-model="${_para.name}_lower_bound">
+            <label class="pl-2 pr-2">~</label>
+            <input type="text" class="form-control col col-lg-3" placeholder="上限"
+                v-model="${_para.name}_upper_bound">
+        </div>
+    </div>
+    </div>
+    `
+}
+function get2LevelOptionHtml(_para) {
+    function getInnerOption(_data){
+        var res =  `<div class="form-group row text-center">
+                        <label class="col-2 pl-0 pr-0 col-form-label text-center">${_data.text}</label>
+                        <div class="col text-left align-self-center">`
+        for (var key in _data.items) {
+            var item = _data.items[key]
+            res += `<div class="form-check form-check-inline">
+                        <input class="form-check-input" type="checkbox" id="${_para.name}_${item.value}"
+                            value="${item.value}" v-model="${_para.name}">
+                        <label class="form-check-label" for="${_para.name}_${item.value}">${item.text}</label>
+                    </div>`
+        }
+        res += `</div>
+            </div>`
+        
+        return res
+    }
+
+    var res = `
+    <div class="form-group row text-center">
+        <div class="col">
+            <h3 class="row text-right pl-1 accordion-toggle" data-toggle="collapse" href="#collapse_${_para.name}"
+                role="button" aria-expanded="false" aria-controls="collapse_${_para.name}">
+                ${_para.text}</h3>
+            <div class="collapse show" id="collapse_${_para.name}">
+                <form>
+    `
+
+    for (var key in _para.data){
+        res += getInnerOption(_para.data[key])
+    }
+
+    res += `</form>
+        </div>
+    </div>
+    </div>`
+    return res
 }
