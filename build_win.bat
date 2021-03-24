abceaec80d8234b8f3a4d1888652e06ba9dd4709
mcgodamn <t7214948ttt@gmail.com>
2021-03-24 23:49:39 +0800

    restore build.bat

---
+++ build_win.bat
@@ -0,0 +1,11 @@
+pip install -r "./requirements.txt"
+npm install -g pkg
+rmdir /s /q "Build"
+mkdir "Build"
+cd "./Build"
+mkdir "bin"
+cd ..
+pyinstaller --add-data "./source/python/chromedriver.exe;." --distpath "./Build/bin" -F -n "crawler" "./source/python/main.py"
+rmdir /s /q "./Build/crawler"
+cd "./source/web"
+pkg --out-path "../../Build" -t node14-win-x64 .
