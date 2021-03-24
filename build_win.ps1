40e711e9b969ded69476d9b0d052a4db623c9580
mcgodamn <t7214948ttt@gmail.com>
2021-03-25 01:22:18 +0800

    change cmd to ps1

--- build_win.ps1
+++ build_win.ps1
@@ -1,8 +1,8 @@
-rmdir /s /q "./Build"
-mkdir "./Build"
+rm -Recurse "./Build"
+md "./Build"
 cd "./Build"
-mkdir "./bin"
+md "./bin"
 cd ..
 pyinstaller --add-data "./source/python/chromedriver.exe;." --distpath "./Build/bin" -F -n "crawler" "./source/python/main.py"
-rmdir /s /q "./Build/crawler"
+rm -Recurse "./Build/crawler"
 pkg --out-path "./Build/" -t node14-win-x64 "./source/web/."
