e20ceaeb19f8aa92336392621f5ed43af7795c02
mcgodamn <t7214948ttt@gmail.com>
2021-03-24 23:56:12 +0800

    update build

--- build_win.bat
+++ build_win.bat
@@ -7,5 +7,4 @@ mkdir "bin"
 cd ..
 pyinstaller --add-data "./source/python/chromedriver.exe;." --distpath "./Build/bin" -F -n "crawler" "./source/python/main.py"
 rmdir /s /q "./Build/crawler"
-cd "./source/web"
-pkg --out-path "../../Build" -t node14-win-x64 .
+pkg --out-path "./Build/" -t node14-win-x64 "./source/web/."
