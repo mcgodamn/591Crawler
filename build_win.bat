07dee2c90ef192e64a8573019ff8568c2aa5e525
mcgodamn <t7214948ttt@gmail.com>
2021-03-25 00:08:49 +0800

    update build

--- build_win.bat
+++ build_win.bat
@@ -1,9 +1,9 @@
 pip install -r "./requirements.txt"
-npm install -g pkg
-rmdir /s /q "Build"
-mkdir "Build"
+npm install --save-dev -g pkg
+rmdir /s /q "./Build"
+mkdir "./Build"
 cd "./Build"
-mkdir "bin"
+mkdir "./bin"
 cd ..
 pyinstaller --add-data "./source/python/chromedriver.exe;." --distpath "./Build/bin" -F -n "crawler" "./source/python/main.py"
 rmdir /s /q "./Build/crawler"
